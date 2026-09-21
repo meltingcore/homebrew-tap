f8311ae7d064235b24ee031007e9dd6422f9ebc5
Bozhidar Zhelev <6785016+grumpper@users.noreply.github.com>
2026-09-21 23:46:04 +0300

    fix malina files typo and ignore .idea

--- Casks/malina.rb
+++ Casks/malina.rb
@@ -2,7 +2,7 @@ cask "malina" do
   version "0.1.1"
   sha256 "b688c83cc71cb1838f98d211f2702a5f5ffbdd3f755ab6439af529a4f5cbadcb"
 
-  url "https://github.com/meltingcore/malina/releases/download/v#{version}/malina-#{version}-macos-universal.zip",
+  url "https://github.com/meltingcore/malina/releases/download/v#{version}/malina-#{version}-macos-universal.zip"
   name "Malina"
   desc "Back up and restore Raspberry Pi systems over SSH"
   homepage "https://malina.meltingcore.dev/"
